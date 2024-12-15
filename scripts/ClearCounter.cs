e1a4d3f0eb5418f6f704862c3e7ec6d752b047e3
XuMingyuan <220243749@seu.edu.cn>
2024-12-15 17:15:25 +0800

    create tomato on the top of the counter.

--- scripts/ClearCounter.cs
+++ scripts/ClearCounter.cs
@@ -1,8 +1,23 @@
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class ClearCounter : MonoBehaviour
 {
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
+    private KichenObject kitchenObject;
+
     public void Interact(){
-        Debug.Log("Interact");
+        if(kitchenObject==null){
+            Transform kichenObjectTransform = Instantiate(kitchenObjectSO.prefabs, counterTopPoint);
+            kichenObjectTransform.localPosition = Vector3.zero;
+
+            kitchenObject = kichenObjectTransform.GetComponent<KichenObject>();
+            kitchenObject.SetClearCounter(this);
+        }
+        else
+            Debug.Log(kitchenObject.GetClearCounter());
     }
 }
